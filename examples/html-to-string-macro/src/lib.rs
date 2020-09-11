f2e6626142831d1792021666fe05659bd51ee2af
stoically <stoically@protonmail.com>
2020-09-12 01:42:27 +0200

    feat(example): support blocks in html-to-string-macro
    Resolves #3

--- examples/html-to-string-macro/src/lib.rs
+++ examples/html-to-string-macro/src/lib.rs
@@ -1,23 +1,30 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{spanned::Spanned, Error};
+use syn::Expr;
 use syn_rsx::{parse, Node, NodeType};
 
-fn walk_nodes(nodes: Vec<Node>) -> Result<String, proc_macro2::TokenStream> {
+fn walk_nodes(nodes: Vec<Node>) -> Result<(String, Vec<Expr>), proc_macro2::TokenStream> {
     let mut out = String::new();
+    let mut values = vec![];
     for node in nodes {
         match node.node_type {
             NodeType::Element => {
                 let name = node.name_as_string().unwrap();
                 out.push_str(&format!("<{}", name));
                 match walk_nodes(node.attributes) {
-                    Ok(html_string) => out.push_str(&html_string),
+                    Ok((html_string, attribute_values)) => {
+                        out.push_str(&html_string);
+                        values.extend(attribute_values);
+                    }
                     Err(error) => return Err(error),
                 }
                 out.push_str(">");
 
                 match walk_nodes(node.children) {
-                    Ok(html_string) => out.push_str(&html_string),
+                    Ok((html_string, children_values)) => {
+                        out.push_str(&html_string);
+                        values.extend(children_values);
+                    }
                     Err(error) => return Err(error),
                 };
 
@@ -26,35 +33,28 @@ fn walk_nodes(nodes: Vec<Node>) -> Result<String, proc_macro2::TokenStream> {
             NodeType::Attribute => {
                 out.push_str(&format!(" {}", node.name_as_string().unwrap()));
                 if node.value.is_some() {
-                    match node.value_as_string() {
-                        Some(value) => out.push_str(&format!("=\"{}\"", &value)),
-                        None => return Err(Error::new(
-                            node.name_span().unwrap(),
-                            "Only String literals as attribute value are supported in this example",
-                        )
-                        .to_compile_error()),
-                    }
+                    out.push_str(r#"="{}""#);
+                    values.push(node.value.unwrap());
                 }
             }
             NodeType::Text => out.push_str(&node.value_as_string().unwrap()),
             NodeType::Block => {
-                return Err(Error::new(
-                    node.value_as_block().unwrap().span(),
-                    "NodeType::Block is not supported in this example",
-                )
-                .to_compile_error())
+                out.push_str("{}");
+                values.push(node.value.unwrap());
             }
         }
     }
 
-    Ok(out)
+    Ok((out, values))
 }
 
 #[proc_macro]
 pub fn html_to_string(tokens: TokenStream) -> TokenStream {
     match parse(tokens) {
         Ok(nodes) => match walk_nodes(nodes) {
-            Ok(html_string) => quote! { #html_string },
+            Ok((html_string, values)) => {
+                quote! { format!(#html_string, #(#values),*) }
+            }
             Err(error) => error,
         },
         Err(error) => error.to_compile_error(),
--- examples/html-to-string-macro/tests/lib.rs
+++ examples/html-to-string-macro/tests/lib.rs
@@ -2,8 +2,10 @@ use html_to_string_macro::html_to_string;
 
 #[test]
 fn test() {
+    let world = "world!";
+    let hi = "hi!";
     assert_eq!(
-        html_to_string! { <div hello="world">"hi"</div> },
-        "<div hello=\"world\">hi</div>",
+        html_to_string! { <div hello={world}>{hi}</div> },
+        r#"<div hello="world!">hi!</div>"#,
     );
 }
