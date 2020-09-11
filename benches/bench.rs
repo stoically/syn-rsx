f9340f3e50953c1743b13930ccab32e9445b1aaf
stoically <stoically@protonmail.com>
2020-09-11 23:31:34 +0200

    bench: more test tokens

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,8 +4,16 @@ use quote::quote;
 fn criterion_benchmark(c: &mut Criterion) {
     let tokens = quote! {
         <div>
-            <button onclick=do_something()>"hi"</button>
-            <p>{ value }</p>
+            <hello world />
+            <div>"String literal"</div>
+            <tag-name attribute-key="value" />
+            <tag:name attribute:key="value" />
+            <tag::name attribute::key="value" />
+            <input type="submit" />
+            <div key=some::value() />
+            <div>{ let block = "in node position"; }</div>
+            <div { let block = "in attribute position"; } />
+            <div key={ let block = "in attribute value position"; } />
         </div>
     };
 
